85a96ad5dbeee72fef6b34822544f27fe03efd71
Reynier Go <39437340+ReynierGo@users.noreply.github.com>
2023-05-08 09:50:59 -0500

    MODIFICACION: Archivo configuracion.

--- Constants.cs
+++ Constants.cs
@@ -2,19 +2,30 @@
 {
     public static class Constants
     {
-        // WebService - Desarrollo 
-        public static string HostUrl = "192.168.58.50:8006";
-        // WebService - Produccion
-        // public static string HostUrl = "192.168.10.213:8081";
+		// Variable de IP productivo para comparar
+		public static string CheckHostUrlProd = "192.168.10.213:8081";
 
-        public static string VersionAppLetra = HostUrl == "192.168.10.213:8081" ? "P" : "D";
-        public static string VersionApp = "v" + VersionTracking.CurrentVersion + VersionAppLetra;
-        public static string APPNombre = "TOVA_APP_ASOCIADOS";
+		// ---
+		// WebService - Desarrollo 
+		// ---
+		// public static string HostUrl = "192.168.58.50:8006"; // Local
+		public static string HostUrl = "192.168.10.178:8081"; // Servidor desarrollo
+
+
+		// ---
+		// WebService - Produccion
+		// ---
+		// public static string HostUrl = "192.168.10.213:8081"; // Se utiliza la misma IP
+
+
+		// Configuracion
+		public static string VersionAppLetra = HostUrl.Equals(CheckHostUrlProd) ? "P" : "D";
+		public static string VersionApp = "v" + VersionTracking.CurrentVersion + VersionAppLetra;
+		public static string APPNombre = "TOVA_APP_ASOCIADOS";
         public static string GUCAplicacionCodigo = "AMA";
         public static string Scheme = "http"; // https or http
         public static string RestUrl = $"{Scheme}://{HostUrl}/api/appconteos";
         public static string ProxyRestUrl = $"{Scheme}://{HostUrl}/api/proxy";
-        public static string VPreciosRestUrl = $"{Scheme}://{HostUrl}/api/appvprecios";
         public static string ControlVersionRestUrl = $"{Scheme}://{HostUrl}/api/control_versiones";
         public static string AsociadosRestUrl = $"{Scheme}://{HostUrl}/api/appasociados";
         public static string AuthRestUrl = $"{Scheme}://{HostUrl}/api/auth";
